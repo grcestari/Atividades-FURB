3dea1792fa1f0673a6648f71606517c3d0169df6
Gabriel Cestari <grcestari@gmail.com>
2023-03-15 20:36:59 -0300

    test

--- exercicios/Un3/ex11.java
+++ exercicios/Un3/ex11.java
@@ -1,18 +1,20 @@
 package exercicios.Un3;
 
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 public class ex11 {
     public static void main(String[] args) {
     Scanner teclado = new Scanner(System.in);
-    DecimalFormat df_2 = new DecimalFormat("0.00");
     
     //ENTRADA
     System.out.print("Informe a temperatura em celcius. ");
     double c = teclado.nextDouble();
 
     //PROCESSO
-    
+    double f = 1.8 * c + 32;
+
+    //SAIDA
+    System.out.println("A temperatura em farenheit e " + f);
+    teclado.close();
     }
 }
