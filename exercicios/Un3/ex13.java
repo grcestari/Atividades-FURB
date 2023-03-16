00f194df190f90d5a3f3dec639a87daf601ba4e1
Gabriel Cestari <grcestari@gmail.com>
2023-03-15 21:15:41 -0300

    12 e 13

---
+++ exercicios/Un3/ex13.java
@@ -0,0 +1,25 @@
+package exercicios.Un3;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class ex13 {
+    public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    DecimalFormat df_2 = new DecimalFormat("0.00");
+
+    //ENTRADA
+    System.out.print("Insira o comprimento da parede (em metros).");
+    double comp = teclado.nextDouble();
+    System.out.print("Insira a largura da parede (em metros).");
+    double larg = teclado.nextDouble();
+
+    //PROCESSO
+    double area = comp * larg;
+    double azl = area * 9;
+
+    //SAIDA
+    System.out.println("Serao necessarios " + df_2.format(azl) + " azulejos para preencher a parede.");
+    teclado.close();
+    }    
+}
