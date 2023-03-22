cdf2ecab7d4eb7206dfc53fe2e4187156b16c449
Gabriel Cestari <grcestari@gmail.com>
2023-03-22 00:52:16 -0300

    bo3

---
+++ exercicios/BeeCrowd/Ex1016.java
@@ -0,0 +1,17 @@
+package exercicios.BeeCrowd;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ex1016 {
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+
+        int dist = teclado.nextInt();
+
+        int min = dist * 2;
+
+        System.out.println(min + " minutos");
+        teclado.close();
+    }
+}
---
+++ exercicios/BeeCrowd/Ex1018.java
@@ -0,0 +1,35 @@
+package exercicios.BeeCrowd;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ex1018 {
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+
+        int dinheiros = teclado.nextInt();
+
+        int n100 = dinheiros / 100;
+        int r100 = dinheiros % 100;
+        int n50 = r100 / 50;
+        int r50 = r100 % 50;
+        int n20 = r50 / 20;
+        int r20 = r50 % 20;
+        int n10 = r20 / 10;
+        int r10 = r20 % 10;
+        int n5 = r10 / 5;
+        int r5 = r10 % 5;
+        int n2 = r5 / 2;
+        int n1 = r5 % 2;
+
+        System.out.println(dinheiros);
+        System.out.println(n100 + " nota(s) de R$ 100,00");
+        System.out.println(n50 + " nota(s) de R$ 50,00");
+        System.out.println(n20 + " nota(s) de R$ 20,00");
+        System.out.println(n10 + " nota(s) de R$ 10,00");
+        System.out.println(n5 + " nota(s) de R$ 5,00");
+        System.out.println(n2 + " nota(s) de R$ 2,00");
+        System.out.println(n1 + " nota(s) de R$ 1,00");
+        teclado.close();
+    }
+}
