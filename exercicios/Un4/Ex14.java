36bbe3645dd289dcd2267c6f2f4ed8bde6d66c43
Gabriel Cestari <grcestari@gmail.com>
2023-04-06 15:57:28 -0300

    06/04/23 15:57

---
+++ exercicios/Un4/Ex14.java
@@ -0,0 +1,40 @@
+package exercicios.Un4;
+
+import java.util.Scanner;
+
+public class Ex14 {
+    public static void main(String[] args) {
+        Scanner kbrd = new Scanner(System.in);
+
+        int dia = kbrd.nextInt();
+        int mes = kbrd.nextInt();
+        int ano = kbrd.nextInt();
+
+        String resp = "";
+
+        if (dia > 0 && dia < 32 && mes > 0 && mes < 13 && ano > 0) {
+            if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
+                resp = "Valida";
+            } else {
+                if (mes != 2 && dia < 31) {
+                    resp = "Valida";
+                } else {
+                    if (mes == 2) {
+                        resp = "Valida";
+                    } else {
+                        if (dia == 29 && ano % 4 == 0 && !(ano % 100 == 0 && ano % 400 != 0)) {
+                            resp = "Valida";
+                        } else {
+                            resp = "Nao valida";
+                        }
+                    }
+                }
+            }
+        } else {
+            resp = "Nao valida";
+        }
+
+        System.out.println(resp);
+        kbrd.close();
+    }
+}
---
+++ exercicios/Un4/Ex15.java
@@ -0,0 +1,26 @@
+package exercicios.Un4;
+
+import java.util.Scanner;
+
+public class Ex15 {
+    public static void main(String[] args) {
+        Scanner kbrd = new Scanner(System.in);
+
+        System.out.print("Insira a quantidade de meses que o funcionario foi admitido. ");
+        int mes = kbrd.nextInt();
+
+        String resp = "";
+
+        if (mes < 12) {
+            resp = "5%";
+        } else {
+            if (mes > 13 && mes < 48) {
+                resp = "7%";
+            } else {
+                resp = "0%";
+            }
+        }
+        System.out.println("Reajuste = " + resp);
+        kbrd.close();
+    }
+}
