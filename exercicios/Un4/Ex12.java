b7d8400e56c86e9f7a1fab825d22cb9119408f81
Gabriel Cestari <grcestari@gmail.com>
2023-04-06 14:08:52 -0300

    06/04/23 14:08

---
+++ exercicios/Un4/Ex12.java
@@ -0,0 +1,32 @@
+package exercicios.Un4;
+
+import java.util.Scanner;
+
+public class Ex12 {
+    public static void main(String[] args) {
+        Scanner kbrd = new Scanner(System.in);
+
+        double lado1 = kbrd.nextDouble();
+        double lado2 = kbrd.nextDouble();
+        double lado3 = kbrd.nextDouble();
+
+        String resp = "";
+
+        if (lado1 < (lado2 + lado3) && lado2 < (lado1 + lado3) && lado3 < (lado1 + lado2)) {
+            if (lado1 == lado2 && lado1 == lado3) {
+                resp = "triangulo equilatero";
+            } else {
+                if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+                    resp = "triangulo isosceles";
+                } else {
+                    resp = "triangulo escaleno";
+                }
+            }
+        } else {
+            resp = "nao e triangulo";
+        }
+
+        System.out.println(resp);
+        kbrd.close();
+    }
+}
---
+++ exercicios/Un4/Ex13.java
@@ -0,0 +1,40 @@
+package exercicios.Un4;
+
+import java.util.Scanner;
+
+public class Ex13 {
+    public static void main(String[] args) {
+        Scanner kbrd = new Scanner(System.in);
+
+        int carta1 = kbrd.nextInt();
+        int carta2 = kbrd.nextInt();
+        int carta3 = kbrd.nextInt();
+
+        int qtCartasBoas = 0;
+
+        if (carta1 == 1 || carta1 == 2 || carta1 == 3) {
+            qtCartasBoas += 1;
+        }
+        if (carta2 == 1 || carta2 == 2 || carta2 == 3) {
+            qtCartasBoas = qtCartasBoas + 1;
+        }
+        if (carta3 == 1 || carta3 == 2 || carta3 == 3) {
+            qtCartasBoas++;
+        }
+
+        String resp = "";
+
+        if (qtCartasBoas == 1) {
+            resp = "TRUCO";
+        }
+        if (qtCartasBoas == 2) {
+            resp = "SEIS";
+        }
+        if (qtCartasBoas == 3) {
+            resp = "NOVE";
+        }
+
+        System.out.println(resp);
+        kbrd.close();
+    }
+}
