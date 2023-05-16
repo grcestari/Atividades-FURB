95161ff01a24cdd319ca8a8a1e11fd1b28221f45
Gabriel Cestari <grcestari@gmail.com>
2023-05-15 23:32:41 -0300

    h

--- exercicios/Un5/Ex10.java
+++ exercicios/Un5/Ex10.java
@@ -1,5 +1,26 @@
 package Un5;
 
 public class Ex10 {
-    
+    public static void main(String[] args) {
+        for (int r = 1; r <= 10; r++) {
+
+            for (int i = 1; i <= 999999; i++)
+
+                for (int h = 1; h <= 99999; h++) {
+
+                    int s = i + h;
+                    String j = i + "" + h;
+                    int k = Integer.parseInt(j);
+
+                    if (Math.pow((s), 2) == k) {
+                        System.out.println(i + " + " + h + " = " + s + " -> " + s + "² = " + k);
+                        r++;
+                    }
+
+                }
+
+        }
+
+    }
+
 }
