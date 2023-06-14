9c5ba5708dbe1d09667db191d689e8b3f08d36d7
Gabriel Cestari <grcestari@gmail.com>
2023-06-14 15:02:21 -0300

    bo 2

--- exercicios/Un6/Uni6Exe10.java
+++ exercicios/Un6/Uni6Exe10.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Uni6Exe10 {
     public static void main(String[] args) {
         Scanner kbrd = new Scanner(System.in);
-        int[] aeeeh = new int[5];
+        int[] aeeeh = new int[50];
 
-        int ent = 0, pos = 4, num = 0, k = 0, h = 4, bubble = 0;
+        int ent = 0, pos = 0, num = 0, k = 0, h = 0, bubble = 0, aux = 0;
         boolean test = false;
 
         while (ent != 8) {
@@ -23,7 +23,7 @@ public class Uni6Exe10 {
                 case 1:
                     System.out.println("1 - Incluir valor" + '\n');
 
-                    if (pos >= 0) {
+                    if (pos < aeeeh.length) {
                         System.out.print("Digite um numero inteiro: ");
 
                         for (int i = 0; i < 1; i++) {
@@ -52,7 +52,8 @@ public class Uni6Exe10 {
                         k = 0;
 
                         aeeeh[pos] = num;
-                        pos--;
+                        test = false;
+                        pos++;
 
                     } else {
                         System.out.println("Vetor lotado! Para adicionar mais numeros apague algum do vetor.");
@@ -60,7 +61,6 @@ public class Uni6Exe10 {
 
                     System.out.println();
                     break;
-
 
                 case 2:
                     System.out.println("2 - Pesquisar valor" + '\n');
@@ -90,7 +90,6 @@ public class Uni6Exe10 {
                     test = false;
                     System.out.println();
                     break;
-
 
                 case 3:
                     System.out.println("3 - Alterar valor" + '\n');
@@ -122,7 +121,6 @@ public class Uni6Exe10 {
                     System.out.println();
                     break;
 
-
                 case 4:
                     System.out.println("4 - Excluir valor" + '\n');
                     System.out.print("Excluir: ");
@@ -136,15 +134,15 @@ public class Uni6Exe10 {
                             System.out.println("\nValor excluido.");
                             aeeeh[k] = 0;
 
-                            while (h > (pos + 1)) {
+                            while (h < (pos - 1)) {
                                 if (aeeeh[h] == 0) {
                                     bubble = aeeeh[h];
-                                    aeeeh[h] = aeeeh[h - 1];
-                                    aeeeh[h - 1] = bubble;
-                                    h = 4;
+                                    aeeeh[h] = aeeeh[h + 1];
+                                    aeeeh[h + 1] = bubble;
+                                    h = 0;
 
                                 } else {
-                                    h--;
+                                    h++;
 
                                 }
 
@@ -162,12 +160,85 @@ public class Uni6Exe10 {
                         System.out.println("\nValor nao existe no vetor.");
                     }
 
-                    pos++;
-                    h = 4;
+                    pos--;
+                    h = 0;
                     k = 0;
                     test = false;
                     System.out.println();
                     break;
+
+                case 5:
+                    System.out.println("5 - Mostrar valores" + '\n');
+
+                    k = pos - 1;
+
+                    while (k >= aux) {
+                        System.out.print(aeeeh[aux]);
+
+                        if (k != aux) {
+                            System.out.print(" ");
+
+                        }
+                        aux++;
+
+                    }
+
+                    aux = 0;
+                    k = 0;
+                    test = false;
+                    System.out.println('\n');
+                    break;
+
+                case 6:
+                    System.out.println("6 - Ordenar valores" + '\n');
+
+                    while (h < (pos - 1)) {
+                        if (aeeeh[h] > aeeeh[h + 1]) {
+                            bubble = aeeeh[h];
+                            aeeeh[h] = aeeeh[h + 1];
+                            aeeeh[h + 1] = bubble;
+                            h = 0;
+
+                        } else {
+                            h++;
+
+                        }
+
+                    }
+
+                    System.out.println("Vetor ordenado.");
+
+                    h = 0;
+                    System.out.println();
+                    break;
+
+                case 7:
+                System.out.println("7 - Inverter valores" + '\n');
+
+                aux = pos - 1;
+
+                    while (h <= aux) {
+                            bubble = aeeeh[h];
+                            aeeeh[h] = aeeeh[aux];
+                            aeeeh[aux] = bubble;
+                            h++;
+                            aux--;
+
+                    }
+
+                    System.out.println("Vetor invertido.");
+
+                    h = 0;
+                    aux = 0;
+                    System.out.println();
+                    break;
+
+                    case 8:
+                    System.out.println("Ate mais!");
+                    break;
+
+                    default:
+                    System.out.println("Opcao invalida! Tente novamente.\n");
 
             }
 
