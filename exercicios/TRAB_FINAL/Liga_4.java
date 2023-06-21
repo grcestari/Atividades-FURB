b5051c773f82e95c29832de79a160470fa0d959b
Gabriel Cestari <grcestari@gmail.com>
2023-06-21 18:56:47 -0300

    hh

---
+++ exercicios/TRAB_FINAL/Liga_4.java
@@ -0,0 +1,11 @@
+package TRAB_FINAL;
+
+import java.util.Scanner;
+
+public class Liga_4 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        teclado.close();
+    }
+}
